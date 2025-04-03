22997381351b193b83d375c799c9c2fe1fe9401b
Abdihakiim121 <cabdixakiin121@gmail.com>
2025-04-03 06:43:39 +0300

    Improved payment charge.

---
+++ src/modules/payments/entities/charge-type.entity.ts
@@ -0,0 +1,20 @@
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { PaymentChargeRequest } from "./payment-charge-request.entity";
+
+@Entity()
+export class ChargeType extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    chargeTypeId: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column({ default: true })
+    isActive: boolean;
+
+    @OneToMany(() => PaymentChargeRequest, charge => charge.chargeType)
+    charges: PaymentChargeRequest[];
+} 
